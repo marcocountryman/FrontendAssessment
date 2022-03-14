d225378a920b140414b25ee3f2c18339ffcca6d5
mcountryman <countryman.marco@gmail.com>
2022-03-14 16:25:40 -0400

    first commit

---
+++ src/components/students.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Students = () => {
+
+    return (
+        <div>
+            This is where the students will go
+        </div>
+    )
+}
+
+export default Students
