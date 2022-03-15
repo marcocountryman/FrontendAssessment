d8ea1ec7981c9c47101985df66f75486eb6c1f8b
mcountryman <countryman.marco@gmail.com>
2022-03-15 17:27:27 -0400

    name check function

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,14 +3,25 @@ import StudentListItem from './StudentListItem';
 
 function StudentList({ students, searchWord }) {
 
+    //I need to check and make sure the names match starting from the first index
+    function nameChecker(firstName, lastName, searchWord) {
+        const checkFirstName = firstName.toLowerCase();
+        const checkLastName = lastName.toLowerCase();
+        const checkSearch = searchWord.toLowerCase();
+
+        for(let i = 0; i < checkSearch.length; i++) {
+            if(checkSearch[i] !== checkFirstName[i] && checkSearch[i] !== checkLastName[i] ) return false;
+        }
+        return true;
+    }
+
     return(
         
         students.filter( (student) => {
-            
+
             if (!searchWord.length) {
                 return student;
-            } else if (student.firstName.toLowerCase().includes(searchWord.toLowerCase()) || 
-            student.lastName.toLowerCase().includes(searchWord.toLowerCase())) {
+            } else if (nameChecker(student.firstName,student.lastName,searchWord)) {
                 return student;
             }
         }).map( (student, idx) => {
